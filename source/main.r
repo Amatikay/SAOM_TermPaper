1402945d2f2b14a6287538f642db44f458bc0972
Amatikay <sengle060@gmail.com>
2023-11-26 19:07:41 +0700

    edit path to data in main.r

--- source/main.r
+++ source/main.r
@@ -5,18 +5,18 @@ library(RSiena)
 ############## Срезы сети #######################
 #                                               #
 #################################################
-co_authorship_before2016 <- as.matrix(read.table("../Data/before_2016.dat"))
-co_authorship_2016_2019 <- as.matrix(read.table("../Data/2016_2019.dat"))
-co_authorship_after2019 <- as.matrix(read.table("../Data/after_2019.dat"))
+co_authorship_before2016 <- as.matrix(read.table("../ParsingData/Data/before_2016.dat"))
+co_authorship_2016_2019 <- as.matrix(read.table("../ParsingData/Data/2016_2019.dat"))
+co_authorship_after2019 <- as.matrix(read.table("../ParsingData/Data/after_2019.dat"))
 
 #################################################
 #                                               #
 ############### Ковариаты #######################
 #                                               #
 #################################################
-experience_2016 <- as.vector(unlist(read.table("../Data/exp_2016.dat")))
-experience_2019 <- as.vector(unlist(read.table("../Data/exp_2019.dat")))
-experience_2023 <- as.vector(unlist(read.table("../Data/exp_2023.dat")))
+experience_2016 <- as.vector(unlist(read.table("../ParsingData/Data/exp_2016.dat")))
+experience_2019 <- as.vector(unlist(read.table("../ParsingData/Data/exp_2019.dat")))
+experience_2023 <- as.vector(unlist(read.table("../ParsingData/Data/exp_2023.dat")))
 
 experience <- cbind(experience_2016, experience_2019,experience_2023)
 
